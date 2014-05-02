ce367be7389ef2e10053c1151720aac1923674e9
javier <jfilippini@alutel.com.uy>
2014-05-02 11:16:29 -0300

    evento boton izquierdo
    Hola Mundo

--- Test/Test/Form1.cs
+++ Test/Test/Form1.cs
@@ -15,5 +15,10 @@ namespace Test
         {
             InitializeComponent();
         }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOLA MUNDO");
+        }
     }
 }
