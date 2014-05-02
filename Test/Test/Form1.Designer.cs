57d6cc32be45202c69c97141ffb765365aad1cfa
Ignacio <igomez@alutel.com.uy>
2014-05-02 11:01:57 -0300

    Rollback 1.1
    Debería incluir un label centrado con texto Hola!

--- Test/Test/Form1.Designer.cs
+++ Test/Test/Form1.Designer.cs
@@ -44,11 +44,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 49);
+            this.label1.Location = new System.Drawing.Point(106, 74);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Hola!";
             // 
             // Form1
             // 
